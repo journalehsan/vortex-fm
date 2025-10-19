30f18cb9bee8cde59143e398a42e800f3c4f91a7
Ehsan Tork <journalehsan@gmail.com>
2025-10-20 03:18:32 +0330

    Fix compilation errors in keyboard shortcuts
    - Import glib::Propagation instead of gtk::Propagation
    - Fix Key struct access by dereferencing instead of calling keyval()
    - Terminal panel F4 shortcut should now compile and work properly

--- src/utils/keyboard.rs
+++ src/utils/keyboard.rs
@@ -1,5 +1,6 @@
 use gtk::prelude::*;
 use gtk::{ApplicationWindow, gio, gdk};
+use glib::Propagation;
 
 pub fn setup_keyboard_shortcuts(window: &ApplicationWindow) {
     // Create action group
@@ -78,12 +79,12 @@ pub fn setup_keyboard_shortcuts(window: &ApplicationWindow) {
     // Add key controller for F4
     let key_controller = gtk::EventControllerKey::new();
     key_controller.connect_key_pressed(|_, key, _, _| {
-        if key.keyval() == gdk::Key::F4 {
+        if *key == gdk::Key::F4 {
             crate::utils::simple_debug::debug_info("KEYBOARD", "F4 key pressed - toggling terminal");
             crate::widgets::terminal_panel::toggle_terminal_panel();
-            gtk::Propagation::Stop
+            Propagation::Stop
         } else {
-            gtk::Propagation::Continue
+            Propagation::Continue
         }
     });
     window.add_controller(key_controller);
